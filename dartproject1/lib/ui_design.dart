5dfbc97194c7982c5aedefbbdc2673a00766a8fd
richard kasomi <167867300+str4wh@users.noreply.github.com>
2025-01-31 20:56:57 +0300

    My Game Third Commit

--- dartproject1/lib/ui_design.dart
+++ dartproject1/lib/ui_design.dart
@@ -21,19 +21,53 @@ class MyGame extends StatelessWidget {
         actions: [
           // Padding(padding: 
           // EdgeInsets.all(24.0)),
-          Icon(
-            Icons.logout,
-            color: Colors.white,
-            size: 24.0,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(
+              Icons.logout,
+              color: Colors.white,
+              size: 24.0,
+            ),
           ),
 
-          Icon(
-            Icons.notification_add,
-            color: Colors.white,
-            size: 24.0,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(
+              Icons.notification_add,
+              color: Colors.white,
+              size: 24.0,
+            ),
           )
         ],
       ),
-    );
+    
+    body: Column(
+      children: [
+        Expanded(
+          child: Container(
+            // height: 300,
+            // width: 300,
+            color: Colors.red,
+          ),
+        ),
+      
+        Expanded(
+          child: Container(
+            // height: 300,
+            // width: 300,
+            color: const Color.fromARGB(255, 114, 61, 57),
+          ),
+        ),
+      
+        Expanded(
+          child: Container(
+            // height: 300,
+            // width: 300,
+            color: const Color.fromARGB(255, 82, 6, 0),
+          ),
+        )
+      ]
+    )
+    );   
   }
 }
